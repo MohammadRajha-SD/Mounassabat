5bb2590ee33b3305985d0d6f0ea1e732fc21878b
MohammadRajha-SD <mohammadrajha2@gmail.com>
2024-12-20 14:45:08 +0200

    some

--- frontend/src/Components/Prestataire/AnnounceForm2.jsx
+++ frontend/src/Components/Prestataire/AnnounceForm2.jsx
@@ -15,7 +15,7 @@ const AnnounceForm = () => {
 
     const handleContinueClick = () => {
         // Check if any required fields are empty
-        if (!title || !price || !description) {
+        if (!title || !description) {
             setFormIncomplete(true);
             setTimeout(() => {
                 setFormIncomplete(false);
@@ -25,9 +25,9 @@ const AnnounceForm = () => {
             setFormIncomplete(false);
 
             localStorage.setItem("title", title);
-            localStorage.setItem("price", price);
+            localStorage.setItem("price", price ?? null);
             localStorage.setItem("description", description);
-            
+
             navigate('/AnnounceForm3');
         }
     };
@@ -36,7 +36,7 @@ const AnnounceForm = () => {
         <>
             <div className='min-h-screen  bg-gray-100 '>
                 <Header2 />
-                
+
                 <div className="flex flex-col md:flex-row pt-40 pb-20 md:py-20  gap-5 mx-2  lg:mx-10">
                     <div className="flex-1 bg-gray-100 ">
                         <section className="bg-white p-6 rounded-xl shadow-none md:shadow-md  ">
@@ -65,7 +65,7 @@ const AnnounceForm = () => {
                                         <div className='bg-gray-100 m-1 rounded-full' style={{ width: '35px', height: '35px', display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
                                             <svg fill="#000000" width="20px" height="20px" viewBox="-2 0 19 19" xmlns="http://www.w3.org/2000/svg" ><path d="m13.842 11.52-4.389 4.388a1.112 1.112 0 0 1-1.567 0l-6.28-6.28a3.027 3.027 0 0 1-.771-1.892l.043-3.681A1.141 1.141 0 0 1 2 2.935L5.67 2.9a3.04 3.04 0 0 1 1.892.773l6.28 6.28a1.112 1.112 0 0 1 0 1.567zM3.826 5.133a.792.792 0 1 0-.792.792.792.792 0 0 0 .792-.792zm6.594 7.348a.554.554 0 0 0 0-.784l-.401-.401a2.53 2.53 0 0 0 .35-.83 1.565 1.565 0 0 0-.397-1.503 1.59 1.59 0 0 0-1.017-.46 2.14 2.14 0 0 0-.75.085h-.002a2.444 2.444 0 0 0-.59.277H7.61a2.677 2.677 0 0 0-.438.357 2.043 2.043 0 0 1-.259.22 1.29 1.29 0 0 1-.329.17h-.002a.835.835 0 0 1-.338.038h-.002a.53.53 0 0 1-.314-.136.539.539 0 0 1-.106-.534 1.54 1.54 0 0 1 .41-.71 1.632 1.632 0 0 1 .23-.165l.03-.019a1.783 1.783 0 0 1 .322-.155.942.942 0 0 1 .325-.06.554.554 0 0 0 0-1.108h-.001a2.058 2.058 0 0 0-.717.132 2.846 2.846 0 0 0-.529.26l-.01.006-.398-.4a.554.554 0 1 0-.784.785l.388.387a2.513 2.513 0 0 0-.347.803 1.644 1.644 0 0 0 .404 1.561 1.622 1.622 0 0 0 .983.456 1.922 1.922 0 0 0 .805-.089 2.372 2.372 0 0 0 .624-.319 3.142 3.142 0 0 0 .398-.339 1.569 1.569 0 0 1 .256-.208 1.381 1.381 0 0 1 .32-.151 1.023 1.023 0 0 1 .348-.038.485.485 0 0 1 .308.139c.05.049.165.165.097.488a1.558 1.558 0 0 1-.413.729 2.476 2.476 0 0 1-.28.219 1.727 1.727 0 0 1-.306.157.687.687 0 0 1-.32.042.554.554 0 1 0-.08 1.106c.052.004.103.005.152.005a1.723 1.723 0 0 0 .685-.134 2.678 2.678 0 0 0 .507-.27l.01-.007.397.398a.555.555 0 0 0 .783 0z" /></svg>
                                         </div>
-                                        <label className="text-md font-medium" ><span className='text-red-500'>*</span>Prix(MAD)</label>
+                                        <label className="text-md font-medium" >Prix(MAD)</label>
                                     </div>
 
                                     <input
@@ -107,9 +107,13 @@ const AnnounceForm = () => {
                                             <label className="text-md font-medium" ><span className='text-red-500'>*</span>Texte de l'annonce</label>
                                         </div>
                                     </div>
-                                    <input id="description"
+                                    <textarea
+                                        id="description"
                                         value={description}
-                                        onChange={(e) => setDescription(e.target.value)} type="text" className="block ml-2 w-full h-64 font-bold text-md py-2 px-2 mt-2  bg-white border border-gray-300 rounded-md  focus:border-yellow-500  focus:outline-none focus:ring" />
+                                        onChange={(e) => setDescription(e.target.value)}
+                                        className="block ml-2 w-full font-bold text-md py-2 px-2 mt-2 bg-white border border-gray-300 rounded-md focus:border-yellow-500 focus:outline-none focus:ring"
+                                        rows="5"
+                                    />
                                 </div>
                                 {formIncomplete && (
                                     <div className="fixed bottom-24 left-0 z-10 w-full flex justify-center">
@@ -126,10 +130,10 @@ const AnnounceForm = () => {
                 </div>
             </div>
 
-            
+
             <div className="fixed right-0 bottom-0 z-10 bg-white w-screen flex justify-end items-center py-2 shadow-md">
-                    <button onClick={handleContinueClick} className='bg-[#e6cf8c] text-white p-2 mx-2 rounded-lg' type='submit' to='/src/Components/AnnounceForm2.jsx'>CONTINUER</button>
-                </div>
+                <button onClick={handleContinueClick} className='bg-[#e6cf8c] text-white p-2 mx-2 rounded-lg' type='submit' to='/src/Components/AnnounceForm2.jsx'>CONTINUER</button>
+            </div>
         </>
     );
 };
