d4fcecac16303b5d563f412520a679d42cbfb4ad
MohammadRajha-SD <mohammadrajha2@gmail.com>
2024-12-25 11:51:48 +0200

    some

--- frontend/src/Components/Loader/Index.jsx
+++ frontend/src/Components/Loader/Index.jsx
@@ -1,13 +1,15 @@
 import React from 'react';
 import './Index.css'; // Import the CSS for the loader
+import LoaderVideo from '../../assets/loader.mp4';
 
 const Index = () => {
+    
     return (
-        <div className="loader">
-            <div className="circle"></div>
-            <div className="circle"></div>
-            <div className="circle"></div>
-            <div className="circle"></div>
+        <div className="loader-container">
+            <video className="loader-video" autoPlay loop muted>
+                <source src={LoaderVideo} type="video/mp4" />
+                Your browser does not support the video tag.
+            </video>
         </div>
     );
 };
